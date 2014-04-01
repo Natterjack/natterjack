8c7b7932090278d5a38d64b8174ddd60e4c095f3
Will Speak <lithiumflame@gmail.com>
2014-04-01 22:03:57 +0100

    Add Boilerplate Code
    some code to be compiled, and a .snooper file to control it all.

---
+++ natterjack/natterjack.cpp
@@ -0,0 +1,14 @@
+// ---------------------------------------------------------------------------
+// 
+//    Project Natterjack - Statically Typed Language, Compiler and Runtime
+// 
+// This file is distributed under the open source MIT license. See LICENSE.md
+// for more info.
+// 
+// ---------------------------------------------------------------------------
+
+#include <iostream>
+
+extern "C" int main(int argc, const char* argv[]) {
+	std::cout << "Hello World!" << std::endl;
+}
