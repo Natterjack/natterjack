e5f0f68e5dc3c5545a947898ef095e5ad85cd0c6
Will Speak <lithiumflame@gmail.com>
2015-04-26 16:16:14 +0100

    Add Number Literals in Alternate Bases
    Adds tokens for binary, octal and hexadecimal literals to the parser.

--- src/main.rs
+++ src/main.rs
@@ -29,4 +29,33 @@ mod test {
         let max_str = format!("{}", ::std::i64::MAX);
         check_literal(&max_str, ::std::i64::MAX);
     }
+
+    #[test]
+    fn test_parse_number_with_whitespace_ignores_whitespace() {
+        check_literal(" 4", 4);
+        check_literal("34 ", 34);
+        check_literal("\t 123456\t ", 123456);
+    }
+
+    #[test]
+    fn test_parse_hex_numbers() {
+        check_literal("0xFF", 255);
+        check_literal("0xa", 10);
+        check_literal("0x123456789abc", 20_015_998_343_868);
+    }
+
+    #[test]
+    fn test_parse_bin_numbers() {
+        check_literal("0b1101", 13);
+        check_literal("0b1111", 15);
+        check_literal("0b111111111111111111111111111111111111111111111111111111111111111", ::std::i64::MAX);
+        check_literal("0b0", 0);
+    }
+
+    #[test]
+    fn test_parse_octal_numbers() {
+        check_literal("0o765432", 256794);
+        check_literal("0o00", 0);
+        check_literal("0o0000001", 1);
+    }
 }
