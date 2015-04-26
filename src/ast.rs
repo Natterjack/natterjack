eaa5c67fecdcc231664ddadd7783b9964f1cf05a
Will Speak <lithiumflame@gmail.com>
2015-04-26 15:42:25 +0100

    Add Parsing of Integer Literals

---
+++ src/ast.rs
@@ -0,0 +1,7 @@
+/// # Expression
+///
+/// Represents a single expression in the AST.
+#[derive(Debug,PartialEq)]
+pub struct Expression {
+    pub value: i64
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#![feature(plugin,collections,str_char)]
+#![plugin(peg_syntax_ext)]
+
+mod ast;
+
+peg_file! parser("grammar.rustpeg");
+
+#[cfg(not(test))]
 fn main() {
-    println!("Natterjack moves to Rust :-)");
+    println!("Nothing to see here, move on :-p");
+}
+
+#[cfg(test)]
+mod test {
+    use super::parser::expression;
+    use super::ast;
+
+    fn check_literal(input: &str, expected: i64) {
+        let parsed = expression(input).unwrap();
+        assert_eq!(parsed, ast::Expression{ value: expected });
+    }
+
+    #[test]
+    fn test_parse_literal_numbers() {
+        check_literal("1", 1);
+        check_literal("1337", 1337);
+        check_literal("100", 100);
+        check_literal("1701", 1701);
+        let max_str = format!("{}", ::std::i64::MAX);
+        check_literal(&max_str, ::std::i64::MAX);
+    }
 }
